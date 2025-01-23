364d8e6fa72f947e4410fe254571913a54d9741f
Abdallah Salah <Abdallah.Salah@ieee.org>
2025-01-24 00:20:25 +0200

    Add project for calculating human years in dog years

---
+++ day2/Dogs years.js
@@ -0,0 +1,11 @@
+const myage = 22
+var earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myage-2
+laterYears*=4
+console.log(earlyYears)
+console.log(laterYears)
+myAgeInDogYears = earlyYears+laterYears
+myname='Abdallah salah'.toLowerCase();
+console.log(`My name is ${myname}. I am ${myage} years old in human years which is ${myAgeInDogYears} old in dog years.
+`)
